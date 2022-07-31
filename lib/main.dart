369facf214852e36d7f8bd5bb073b04c7bcad152
Panachai <panachai.ny@gmail.com>
2022-08-01 00:05:41 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
@@ -44,13 +45,34 @@ class _RandomWordsState extends State<RandomWords> {
 
         final index = i ~/ 2;
         if (index >= _suggestions.length) {
+          Iterable<WordPair> iterable = [
+            WordPair("sdasd", "2"),
+          ];
+
           _suggestions.addAll(generateWordPairs().take(10));
         }
+
+        final alreadySaved = _saved.contains(_suggestions[index]);
+
         return ListTile(
           title: Text(
             _suggestions[index].asPascalCase,
             style: _biggerFont,
           ),
+          trailing: Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,
+            semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+          ),
+          onTap: (() {
+            setState(() {
+              if (alreadySaved) {
+                _saved.remove(_suggestions[index]);
+              } else {
+                _saved.add(_suggestions[index]);
+              }
+            });
+          }),
         );
       },
     );
